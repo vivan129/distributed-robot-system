1349261ce3a059ddd9090f8d23387002fd62a8fe
vivan129 <vivangpt25@gmail.com>
2026-01-01 12:26:38 +0530

    Add hardware module init file

--- pi_client/hardware/__init__.py
+++ pi_client/hardware/__init__.py
@@ -1,1 +1,13 @@
-"""Hardware Interface Modules"""
+"""Hardware interface modules for Raspberry Pi."""
+
+from .motor_controller import MotorController
+from .camera_streamer import CameraStreamer
+from .lidar_streamer import LidarStreamer
+from .ultrasonic_sensor import UltrasonicSensor
+
+__all__ = [
+    'MotorController',
+    'CameraStreamer',
+    'LidarStreamer',
+    'UltrasonicSensor'
+]
