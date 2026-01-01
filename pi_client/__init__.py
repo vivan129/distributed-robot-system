af08f6a0ecec8b220cc919f2f9e8a7ded79329d5
vivan129 <vivangpt25@gmail.com>
2026-01-01 12:40:47 +0530

    Add PC dashboard template and Pi client hardware modules

---
+++ pi_client/__init__.py
@@ -0,0 +1,3 @@
+"""Raspberry Pi Client for Distributed Robot System."""
+
+__version__ = "1.0.0"
--- pi_client/hardware/__init__.py
+++ pi_client/hardware/__init__.py
@@ -1,13 +1,1 @@
 """Hardware interface modules for Raspberry Pi."""
-
-from .motor_controller import MotorController
-from .camera_streamer import CameraStreamer
-from .lidar_streamer import LidarStreamer
-from .ultrasonic_sensor import UltrasonicSensor
-
-__all__ = [
-    'MotorController',
-    'CameraStreamer',
-    'LidarStreamer',
-    'UltrasonicSensor'
-]
